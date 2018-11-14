6efaf27b798ab0aebbcfa3275fa8ac29df38c744
ItsMeMalario <Witaj@asdf.pl>
2018-11-14 19:42:42 +0100

    dodano

---
+++ zadania.py
@@ -0,0 +1,34 @@
+"""
+	Zadanie 1
+	Utwórz zmienną o dowolnej nazwie i wartości typu float
+"""
+zmienna = float(45)
+zmienna = 7.5
+
+"""
+	Zadanie 2
+	Napisz pętle która wypiszie parzyste liczby od 0 do 25
+"""
+for x in range(0,25,2):
+    print(x)
+
+"""
+	Zadanie 3
+	Z listy poniżej wypisz do konsoli:
+	- 8-my element oraz
+	- listę zawierające elemęty od 4 do 9(kawałek listy)
+	- długość listy
+"""
+lista = [14,"owca","owca2","19",24,18,29,84,9019,True,False,
+		 [1,2,3,4,5],"ojoj jak dużo elemętów ma ta lista","co zorbić"]
+
+
+print (lista[7])
+print (lista[3:9])
+print (len(lista))
+
+"""
+    Zadanie 4
+    Utwórz zmienną będącą tuplem
+    Zmień wartość pierwszego elemętu tupla
+"""
